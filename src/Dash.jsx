1d7dae5c227e7ee2d76fc9e7b2cec3ac487819c1
fdavison1 <fred.noreply@gmail.com>
2019-11-20 15:57:58 -0700

    debugging 1

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -5,11 +5,6 @@ import Project from './Project'
 
 export default class Dash extends React.Component {
     state = {
-        tasksOriginal: {
-            '7': { id: '7', content: 'clean the bathroom' },
-            '8': { id: '8', content: 'mop the floor' },
-            '9': { id: '9', content: 'paint the fence' }
-        },
         tasks: [
             { task_id: 1, content: 'test' },
             { task_id: 2, content: 'test' },
@@ -44,12 +39,51 @@ export default class Dash extends React.Component {
         }
     
         onDragEnd = result => {
-            //
+            const {destination, source, draggableId } = result
+            
+            //if destination is null (dragged outside of DragDropContext):
+            if(!destination){
+                return
+            }
+
+            //if dropped in same location (no action required): 
+            if(
+                destination.droppableId === source.droppableId &&
+                destination.index === source.index
+            ){
+                return
+            }
+
+            //look up project by droppableId?????
+            const project = this.state.projects.find(el => el.project_id === +source.droppableId)
+            // console.log(project)
+            const newTaskIds = Array.from(project.taskIds)
+            newTaskIds.splice(source.index, 1)
+            newTaskIds.splice(destination.index, 0, +draggableId)
+
+            // return console.log(newTaskIds)
+
+            const newProject = {
+                ...project, 
+                taskIds: newTaskIds
+            }
+            // console.log(newProject) // ----> good taskIds
+
+            // const newState = {
+            //     ...this.state,
+            //     projects: this.state.projects.splice(0, 1, newProject)
+            // }
+            // console.log(this.state.projects)
+            this.setState({
+                projects: [{...this.state.projects}]
+            })
+            // this.getTasks()
+            // console.log(this.state.projects)
         }
 
 
     render() {
-        // console.log(this.state.projects)
+
 
 
 
@@ -60,16 +94,17 @@ export default class Dash extends React.Component {
 
                
                  <div>
-                {this.state.projects.map((projectId, index) => 
+                {this.state.projects.map((projectId, index) =>  
                     {
+                    console.log(this.state.projects[index])
                     const project = this.state.projects[index]
-                    // console.log(project.project_id)
+                    // console.log(project)
                     const tasks = project.taskIds.map((taskId, index) => this.state.tasks[index])
 
 
 
                     //  return project.title
-                    return <Project key={project.id} project={project} tasks={tasks} index={index}/>
+                    return <Project key={project.project_id} project={project} tasks={tasks} index={index}/>
                     
                     }
                     
